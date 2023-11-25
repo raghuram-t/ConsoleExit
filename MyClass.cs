81626fcf3ab1256e116f310d192aeb0e759b83b6
unknown <raghu@itorbit.in>
2023-11-26 02:51:41 +0530

    First commit console

---
+++ MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleExit4
+{
+    public class MyClass
+    {
+        public static void myHandler(object sender, ConsoleCancelEventArgs args) {
+
+            Console.WriteLine("Someone pressed Ctrl C ?");
+
+            args.Cancel = true;
+
+            Console.WriteLine("Initiating routines before closign the program....");
+
+            // Run my code here...
+
+            Console.WriteLine("Done !!!");
+
+            // Run the code here to exit the console app properly...
+
+            Console.WriteLine("Exiting the Application in 3 seconds");
+            Thread.Sleep(3000);
+            Environment.Exit(0);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleExit4;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+ConsoleKeyInfo cki;
+
+Console.Clear();
+
+// Establish an event handler to process key press events
+
+Console.CancelKeyPress += new ConsoleCancelEventHandler(ConsoleExit4.MyClass.myHandler);
+Console.WriteLine("I am doing some task");
+Thread.Sleep(10000);
+
